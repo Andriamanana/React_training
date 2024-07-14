5cceedb1b9f204595f11bc236937ac7d9e315759
Toavina Andriamanana <a.mtoavina@gmail.com>
2024-07-14 20:04:12 +0400

    init learning react

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="user-story">
+        <ul>
+          <li>London</li>
+          <li>Paris</li>
+          <li>Tokyo</li>
+        </ul>
+      </nav>
+      
+      <article >
+        <h1>London</h1>
+        <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+        <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+      </article>
+
+      <nav className="user">
+        <ul>
+          <li>London</li>
+          <li>Paris</li>
+          <li>Tokyo</li>
+        </ul>
+      </nav>
+    </div>  
+  );
+}
+
+export default App;
